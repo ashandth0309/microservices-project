2e1c219ff1516bec149747e5505836fc5f979da4
ashandth0309 <ashandth0309@gmail.com>
2026-03-22 11:01:00 +0530

    app.js update

--- order-service/src/app.js
+++ order-service/src/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("../swagger");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
+// Routes
 app.use("/api", require("./routes/orderRoutes"));
 
-app.listen(3002, () => console.log("Order Service running"));
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Root route (to avoid "Cannot GET /")
+app.get("/", (req, res) => {
+  res.send("Order Service is running 🚀");
+});
+
+const PORT = 3002;
+
+app.listen(PORT, () => {
+  console.log(`Order Service running on port ${PORT}`);
+});
