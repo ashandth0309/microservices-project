4a7f3b01014625ba68b44a02008b8da02ea78e19
ashandth0309 <ashandth0309@gmail.com>
2026-03-22 12:39:23 +0530

    updating swagger with path

--- order-service/swagger.js
+++ order-service/swagger.js
@@ -1,9 +1,10 @@
 const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
 
 module.exports = swaggerJsdoc({
   definition: {
     openapi: "3.0.0",
     info: { title: "Order Service API", version: "1.0.0" }
   },
-  apis: ["./src/routes/*.js"]
+  apis: [path.join(__dirname, "./src/routes/*.js")]
 });
