390718f78f988b94fbfc6baca11a459cdbae3d26
ashandth0309 <ashandth0309@gmail.com>
2026-03-22 12:53:13 +0530

    final update

--- order-service/src/app.js
+++ order-service/src/app.js
@@ -3,6 +3,10 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("../swagger");
 require("dotenv").config();
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+const customJs = [
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui-bundle.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui-standalone-preset.js",
+];
 
 const app = express();
 app.use(express.json());
@@ -15,8 +19,10 @@ app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, {
-    customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
     customCssUrl: CSS_URL,
+    customJs: customJs,
   })
 );
 
--- order-service/swagger.js
+++ order-service/swagger.js
@@ -6,5 +6,5 @@ module.exports = swaggerJsdoc({
     openapi: "3.0.0",
     info: { title: "Order Service API", version: "1.0.0" }
   },
-  apis: [path.join(__dirname, "./src/routes/*.js")]
+  apis: [path.join(__dirname, "./src/routes/orderRoutes.js")]
 });
